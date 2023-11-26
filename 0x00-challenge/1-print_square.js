9904c7da08e8a561debf655538f69d74cebdcedc
tee <tee@Kinyi.Kinyi>
2023-11-27 01:05:56 +0300

    l

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,19 +6,17 @@
     of the program.
 */
 
+const size = process.argv[2];
 
-if (process.argv.length <= 2) {
-    process.stderr.write("Missing argument\n");
-    process.stderr.write("Usage: ./1-print_square.js <size>\n");
-    process.stderr.write("Example: ./1-print_square.js 8\n");
-    process.exit(1)
+if (!size || isNaN(size) || size <= 0) {
+  console.log('Usage: ./1-print_square.js <size>');
+  process.exit(1);
 }
 
-size = parseInt(process.argv[2], 10)
-
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
-    }
-    process.stdout.write("\n");
+for (let i = 0; i < size; i++) {
+  let row = '';
+  for (let j = 0; j < size; j++) {
+    row += '#';
+  }
+  console.log(row);
 }
